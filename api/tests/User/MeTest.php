4c79bf2f0be6751156cf58a9bfc6986ece9db79c
leocavalcante <lc@leocavalcante.com>
2019-10-28 18:06:26 -0300

    Add simple test

---
+++ api/tests/User/MeTest.php
@@ -0,0 +1,21 @@
+<?php declare(strict_types=1);
+
+namespace Multi\Test\User;
+
+use Multi\Context;
+use Multi\User;
+use Multi\User\Me;
+use PHPUnit\Framework\TestCase;
+
+class MeTest extends TestCase
+{
+    public function testMe()
+    {
+        $context = new Context();
+        $context->user = new User();
+
+        $me = new Me();
+
+        $this->assertSame($context->user, $me([], [], $context));
+    }
+}
