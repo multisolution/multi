061ac0e52ca4280108519c953ec685a28c754144
leocavalcante <lc@leocavalcante.com>
2019-11-04 15:20:46 -0300

    Add front-end project boot from Next.js

---
+++ web/pages/index.tsx
@@ -0,0 +1,12 @@
+import {NextPage} from "next";
+import React from "react";
+
+const Index: NextPage = () => {
+  return (
+    <article>
+      Hello World!!!
+    </article>
+  )
+};
+
+export default Index;
