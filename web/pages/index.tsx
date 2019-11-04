c868dcc6cf875b95ce380894430046beba8addb8
Carlos William <carloswilliamds@gmail.com>
2019-11-04 20:45:29 -0200

    base layout and styles default

--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -1,11 +1,26 @@
 import {NextPage} from "next";
 import React from "react";
+import Link from "next/link";
+import Button from "../components/button";
+import GlobalStyle, {Container, Section} from '../components/global-styled';
+import Layout from "../components/layout";
+
 
 const Index: NextPage = () => {
   return (
-    <article>
-      Hello World!!!
-    </article>
+    <Layout>
+      <Section>
+        <Container>
+            <header>
+              <h1 style={{color:"red"}}>INDEX</h1> 
+
+              <Link href="/meeting-rooms">
+              <Button>Home</Button>
+            </Link>
+            </header>    
+          </Container>
+      </Section>
+    </Layout>
   )
 };
 
