212dd03fc345309fc65df1a7c51274353d7e1250
Dan <dan@dopaminamob.com.br>
2019-11-05 15:56:48 -0300

    done create user

--- web/pages/signup.tsx
+++ web/pages/signup.tsx
@@ -1,3 +1,4 @@
+import { useApolloClient, useMutation } from "@apollo/react-hooks";
 import { NextPage } from "next";
 import React, { useRef, useState } from "react";
 import Button from "../components/button";
@@ -5,6 +6,25 @@ import { Container, Section } from "../components/global-style";
 import Layout from "../components/layout";
 import Input from "../components/input";
 import { Column } from "../components/column";
+import { withApollo } from "../lib/apollo";
+import gql from "graphql-tag";
+
+type User = {
+  id: string;
+  email: string;
+  role: Role;
+};
+
+enum Role {
+  ADMINISTRATOR = "ADMINISTRATOR",
+  COLLABORATOR = "COLLABORATOR"
+}
+
+type UserInput = {
+  email: string;
+  password: string;
+  role: Role;
+};
 
 const Signup: NextPage = () => {
   var emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
@@ -12,7 +32,20 @@ const Signup: NextPage = () => {
   const pass = useRef<HTMLInputElement>(null);
   const confirmpass = useRef<HTMLInputElement>(null);
   const [validEmail, setValidEmail] = useState(false);
+  const [passValue, setPassValue] = useState("");
+  const [emailValue, setEmailValue] = useState("");
   const [validPass, setValidPass] = useState(false);
+  const [emailErrorMessage, setEmailError] = useState("");
+  const [passErrorMessage, setpassError] = useState("");
+  const [createUser] = useMutation<{ createUser: User }, { input: UserInput }>(
+    gql`
+      mutation CreateUser($input: UserInput!) {
+        createUser(input: $input) {
+          id
+        }
+      }
+    `
+  );
 
   return (
     <>
@@ -23,7 +56,7 @@ const Signup: NextPage = () => {
               <Column>
                 <div>
                   <Input
-                    onChange={onEmailChange}
+                    onChange={validateEmail}
                     type="text"
                     placeholder="Email"
                     ref={email}
@@ -34,7 +67,7 @@ const Signup: NextPage = () => {
                 </div>
                 <div>
                   <Input
-                    onChange={onPassChange}
+                    onChange={validatePass}
                     type="password"
                     placeholder="Senha"
                     ref={pass}
@@ -45,7 +78,7 @@ const Signup: NextPage = () => {
                 </div>
                 <div>
                   <Input
-                    onChange={onPassChange}
+                    onChange={validatePass}
                     type="password"
                     placeholder="Confirmar senha"
                     ref={confirmpass}
@@ -59,40 +92,86 @@ const Signup: NextPage = () => {
                 </Button>
               </Column>
             </form>
+            {renderError()}
           </Container>
         </Section>
       </Layout>
     </>
   );
 
-  function onEmailChange() {
+  function renderError() {
+    return (
+      <div>
+        <b>{emailErrorMessage} </b>
+        <br></br>
+        <b>{passErrorMessage}</b>
+      </div>
+    );
+  }
+
+  function validateEmail() {
     if (email.current !== null) {
       if (emailRegex.test(String(email.current.value).toLocaleLowerCase())) {
+        setEmailError("");
+        setEmailValue(email.current.value);
         setValidEmail(true);
       } else {
+        setEmailValue("");
+        setEmailError("Email inválido");
         setValidEmail(false);
       }
     }
   }
 
-  function onPassChange() {
-    if (confirmpass.current !== null && pass.current !== null) {
-      if (
-        confirmpass.current.value === pass.current.value &&
-        pass.current.value.length > 5
-      ) {
+  function validatePass() {
+    if (
+      confirmpass.current !== null &&
+      pass.current !== null &&
+      validateMinimumPassSize()
+    ) {
+      if (confirmpass.current.value === pass.current.value) {
+        setpassError("");
+        setPassValue(confirmpass.current.value);
         setValidPass(true);
       } else {
+        setPassValue("");
+        setpassError("As senhas não coincidem");
         setValidPass(false);
       }
     }
   }
-  function sendForm(event: React.MouseEvent<HTMLButtonElement>) {
+
+  function validateMinimumPassSize() {
+    if (confirmpass.current !== null) {
+      if (confirmpass.current.value.length < 5) {
+        setpassError("A senha deve ter ao menos 6 caracteres");
+        return false;
+      } else {
+        setpassError("");
+        return true;
+      }
+    }
+  }
+  async function sendForm(event: React.MouseEvent<HTMLButtonElement>) {
     event.preventDefault();
+    validatePass();
+    validateEmail();
+    validateMinimumPassSize();
+
     if (validEmail && validPass) {
       console.log("Pass");
+      const result = await createUser({
+        variables: {
+          input: {
+            email: emailValue,
+            password: passValue,
+            role: Role.COLLABORATOR
+          }
+        }
+      });
+      console.log(result);
     }
   }
 };
 
-export default Signup;
+export default withApollo(Signup);
