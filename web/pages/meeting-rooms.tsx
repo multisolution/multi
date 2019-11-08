5c79a98a593916f14cf15f8c5fac0df17dbfe4c4
Carlos William <carloswilliamds@gmail.com>
2019-11-08 21:23:56 -0200

    merging

--- web/pages/meeting-rooms.tsx
+++ web/pages/meeting-rooms.tsx
@@ -8,6 +8,7 @@ import gql from "graphql-tag";
 import { MeetingRoom } from "../lib/models";
 import { withApollo } from "../lib/apollo";
 import Calendar from "../components/calendar";
+import { CalendarDayBlock } from "../components/calendar-styles";
 
 const MeetingRooms: NextPage = () => {
   return (
@@ -19,61 +20,6 @@ const MeetingRooms: NextPage = () => {
       </Layout>
     </>
   );
-
-  function renderHours() {
-    return (
-      <Column>
-        <div>Horários</div>
-        {Array.from({ length: 24 }).map((_, index: number) => {
-          return <CalendarDayBlock>{index + ':00'}</CalendarDayBlock>
-        })}
-      </Column>
-    );
-  }
-
-  function renderCalenddar() {
-    return calendarData.map((data: any, index: number) => (
-      <Column>
-        <div>{data.date}</div>
-        {baseCalendar(data.mettings)}
-      </Column>
-    ));
-  }
-
-  function baseCalendar(mettings: Meeting[]) {
-    return (
-      <Column>
-        {Array.from({ length: 24 }).map((_, index: number) => {
-          <div>{index}</div>;
-          const hasMeetings = mettings.filter(meeting => {
-            return Math.random() > 0.5; //index>= meeting.startsAt && index< meeting.endsAt
-          });
-
-          if (hasMeetings.length > 0) {
-            return <CalendarDayBlock bgColor='#aabbcc'>{`${hasMeetings.length} reuniões`}</CalendarDayBlock>;
-          }
-          return <CalendarDayBlock>disponível</CalendarDayBlock>;
-        })}
-      </Column>
-    );
-  }
-
-  function renderTable() {
-    if (getRooms.data) {
-      return getRooms.data.meetingRooms.map((room: MeetingRoom, index: number) => (
-        <Room id={room.id} onClick={meetingRoomClickHandler} bgColor={room.color} key={room.id}>
-          <div>
-            <b>{room.id} </b>
-            <b>{room.description}</b>
-          </div>
-        </Room>
-      ));
-    }
-  }
-  
-  function meetingRoomClickHandler(event: React.MouseEvent<HTMLElement, MouseEvent>) {
-    console.log(event.currentTarget.id);
-  }
 };
 
 export default withApollo(MeetingRooms);
