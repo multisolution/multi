5f671ba0c4793d8c9cf98dafc8e9815a82e35853
leocavalcante <lc@leocavalcante.com>
2019-11-08 16:59:10 -0300

    Remove background from the header button

--- web/components/box-header.tsx
+++ web/components/box-header.tsx
@@ -1,63 +1,59 @@
-import React, {useState, useCallback, useEffect, MouseEvent} from "react";
-import Label from "./label";
-import styled from "styled-components"
-import Link from 'next/link'
-const ContainerUsersBox = styled.div`
-    display: flex;
-    flex-direction: row;
+import React from "react";
+import styled from "styled-components";
+import Link from "next/link";
 
-`
+const ContainerUsersBox = styled.div`
+  display: flex;
+  flex-direction: row;
+`;
 
 const UserBox = styled.div`
-    display: flex;
-    flex-direction: column;
-    margin-left: 20px;
+  display: flex;
+  flex-direction: column;
+  margin-left: 20px;
 
-    button{
-        border: none;
-        outline: none;
-        font-weight:bold;
-        font-size: 16px;
-    
-        &:hover{
-            color: #bad531;
-            cursor: pointer;
-        }
+  button {
+    border: none;
+    outline: none;
+    font-weight: bold;
+    font-size: 16px;
+    background: none;
 
+    &:hover {
+      color: #bad531;
+      cursor: pointer;
     }
-    
-`
-    const BoxHeader = () =>{
+  }
+`;
+const BoxHeader = () => {
+  return (
+    <ContainerUsersBox>
+      <UserBox>
+        <span>Usuário/Admin</span>
+      </UserBox>
 
-    return(
-        <ContainerUsersBox>
-            <UserBox>
-                <span>Usuário/Admin</span>
-            </UserBox>
-            
-            <UserBox>
-                <Link href="/meeting-rooms">
-                    <button>Calendario</button>
-                </Link>
-            </UserBox>
-            <UserBox>
-                <Link href="/create-user">
-                    <button>Cadastrar Usuário</button>
-                </Link>
-            </UserBox>
-            
-            <UserBox>
-                <Link href="/index">
-                <button>Perfil</button>
-                </Link>
-            </UserBox>
+      <UserBox>
+        <Link href="/meeting-rooms">
+          <button>Calendario</button>
+        </Link>
+      </UserBox>
+      <UserBox>
+        <Link href="/create-user">
+          <button>Cadastrar Usuário</button>
+        </Link>
+      </UserBox>
 
-            <UserBox>
-                <button>Sair</button>
-            </UserBox>
-        </ContainerUsersBox>
-    )
-}
+      <UserBox>
+        <Link href="/index">
+          <button>Perfil</button>
+        </Link>
+      </UserBox>
 
+      <UserBox>
+        <button>Sair</button>
+      </UserBox>
+    </ContainerUsersBox>
+  );
+};
 
-export default BoxHeader
+export default BoxHeader;
