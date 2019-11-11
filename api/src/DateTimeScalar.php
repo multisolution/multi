cb2e0f5ce98c94274a24e9502d2861fd8f90e98b
leocavalcante <lc@leocavalcante.com>
2019-11-11 19:31:53 -0300

    Default timezone

--- api/src/DateTimeScalar.php
+++ api/src/DateTimeScalar.php
@@ -3,7 +3,6 @@
 namespace Multi;
 
 use DateTime;
-use DateTimeZone;
 use GraphQL\Type\Definition\CustomScalarType;
 
 class DateTimeScalar extends CustomScalarType
@@ -21,6 +20,6 @@ class DateTimeScalar extends CustomScalarType
 
     public function parseValue($value)
     {
-        return DateTime::createFromFormat(self::FORMAT, $value, new DateTimeZone('UTC'));
+        return DateTime::createFromFormat(self::FORMAT, $value);
     }
 }
