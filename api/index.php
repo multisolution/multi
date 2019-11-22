f8313e30c8ef9d850341cb14c1aab1ddcdc8f7d0
leocavalcante <lc@leocavalcante.com>
2019-11-22 14:09:46 -0300

    Applying better API for GraphQL with Subscriptions

--- api/index.php
+++ api/index.php
@@ -1,6 +1,4 @@
-<?php
-
-declare(strict_types=1);
+<?php declare(strict_types=1);
 
 namespace Multi;
 
@@ -8,65 +6,53 @@ use Firebase\JWT\JWT;
 use GraphQL\Error\{Debug, FormattedError};
 use Monolog\Handler\ErrorLogHandler;
 use Monolog\Logger;
+use Multi\Event\Dispatcher;
 use Multi\Http\IcsHandler;
 use Multi\User\User;
+use SendGrid;
 use Sentry\ClientBuilder;
 use Sentry\Monolog\Handler;
 use Sentry\State\Hub;
 use Siler\Dotenv as Env;
 use Siler\Monolog as Log;
 use Siler\Route;
-use Siler\Container;
+use Swoole\Runtime;
 use Throwable;
-
-use const Siler\Route\DID_MATCH;
-use const Siler\Swoole\SWOOLE_HTTP_REQUEST;
-use const Siler\Swoole\SWOOLE_HTTP_REQUEST_ENDED;
-use const Siler\Swoole\SWOOLE_HTTP_RESPONSE;
-
 use function Siler\Encoder\Json\decode;
 use function Siler\Functional\Monad\maybe;
 use function Siler\GraphQL\{debug, execute, schema, subscriptions_at, subscriptions_manager};
+use function Siler\Swoole\{bearer, cors, http_server_port, json, raw};
 use function Siler\Swoole\graphql_subscriptions;
-use function Siler\Swoole\{bearer, cors, http, json, raw};
-
-
-
-
 
 $base_dir = __DIR__;
 require_once "$base_dir/vendor/autoload.php";
 
-
 Log\handler(new Handler(new Hub(ClientBuilder::create(['dsn' => Env\env('SENTRY_DSN')])->getClient()), Logger::WARNING));
 Log\handler(new ErrorLogHandler());
 
+$dbs = include "$base_dir/app/dbs.php";
 $type_defs = file_get_contents("$base_dir/schema.graphql");
 $resolvers = require_once "$base_dir/resolvers.php";
 $schema = schema($type_defs, $resolvers);
 
+$dispatcher = new Dispatcher();
+$dispatcher->add(new SendGridListener(new SendGrid(Env\env('SENDGRID_API_KEY')), Env\env('SENDGRID_API_FROM')));
 
-
-$context = require_once "$base_dir/context.php";
-
-
-
-
+$context = new Context();
+$context->debug = Env\bool_val('APP_DEBUG', false);
+$context->db = $dbs[Env\env('APP_DB_USE', 'in_memory')](Env\env('APP_DB_URI'));
+$context->appKey = Env\env('APP_KEY');
+$context->id = new UniqueId();
+$context->messages = new InMemoryMessages();
+$context->dispatcher = $dispatcher;
 
 $icsHandler = new IcsHandler($context);
 
+Runtime::enableCoroutine();
 debug($context->debug ? Debug::INCLUDE_DEBUG_MESSAGE : 0);
-
-
 subscriptions_at('ws://localhost:8001');
 
-
-
-$handler = function ($request, $response) use ($schema, $context, $icsHandler) {
-    Container\set(DID_MATCH, false);
-    Container\set(SWOOLE_HTTP_REQUEST_ENDED, false);
-    Container\set(SWOOLE_HTTP_REQUEST, $request);
-    Container\set(SWOOLE_HTTP_RESPONSE, $response);
+$handler = function () use ($schema, $context, $icsHandler) {
     if (Route\get('/meetings/{meeting_id}.ics', $icsHandler)) {
         return;
     }
@@ -77,19 +63,19 @@ $handler = function ($request, $response) use ($schema, $context, $icsHandler) {
             return $context->db->userById($token->userId);
         })->return();
 
-        $result = execute($schema, decode(raw()), [], $context);
+        $result = raw() ? execute($schema, decode(raw()), [], $context) : null;
     } catch (Throwable $exception) {
-        Log\error($exception->getMessage(), ['exception' => $exception]);
+        Log\error($exception->getMessage(), ['exception' => $exception, 'trace' => $exception->getTrace()]);
         $result = FormattedError::createFromException($exception);
     } finally {
-        cors('*', 'authorization, content-type');
+        cors();
         json($result);
     }
 };
 
 $manager = subscriptions_manager($schema, [], [], $context);
-$subscriptions =  graphql_subscriptions($manager, 8001);
-$server =  $subscriptions->addlistener('0.0.0.0', 8000, SWOOLE_SOCK_TCP);
-$server->set(['open_http_protocol' => true]);
-$server->on('request', $handler);
-$subscriptions->start();
+$server = graphql_subscriptions($manager, 8001);
+http_server_port($server, $handler, 8000);
+
+$server->start();
+
