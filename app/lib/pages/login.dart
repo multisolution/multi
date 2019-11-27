a357f48f03443268b9fce36c04ddfdf3a0276f86
Dannylo Dangel <dan@dopaminamob.com.br>
2019-11-27 12:29:27 -0300

    Grapql tests on app

--- app/lib/pages/login.dart
+++ app/lib/pages/login.dart
@@ -1,4 +1,7 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:graphql_flutter/graphql_flutter.dart';
 
 class Login extends StatefulWidget {
@@ -10,41 +13,49 @@ class _LoginState extends State<Login> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Title'),
-      ),
-      body: testMutation(),
-    );
+        appBar: AppBar(
+          title: Text('Title'),
+        ),
+        body: Column(
+          children: <Widget>[
+            FutureBuilder(
+              builder: (BuildContext context, AsyncSnapshot<String> snapshot) {
+                if (snapshot.hasData) {
+                  print(snapshot.data);
+                  return Container();
+                  // return Mutation(
+                  //   options: MutationOptions(
+                  //     document: snapshot.data,
+                  //   ),
+                  //   builder: (
+                  //     RunMutation runMutation,
+                  //     QueryResult result,
+                  //   ) {
+                  //     return FloatingActionButton(
+                  //       onPressed: () => runMutation({
+                  //         "email": "multi@multisolution.art.br",
+                  //         "password": "multi"
+                  //       }),
+                  //       tooltip: 'Star',
+                  //       child: Text(result.data.toString()),
+                  //     );
+                  //   },
+                  //   onCompleted: (dynamic result) {
+                  //     print('onComplpeted');
+                  //     print(result.data);
+                  //   },
+                  // );
+                } else {
+                  return Container();
+                }
+              },
+              future: loadAsset(),
+            )
+          ],
+        ));
   }
 
-  String login = """
-  mutation {
-  signIn(email:"multi@multisolution.art.br" , password:"multi")
-}
-""";
-
-  Widget testMutation() {
-    return Mutation(
-      options: MutationOptions(
-        document: login,
-      ),
-      builder: (
-        RunMutation runMutation,
-        QueryResult result,
-      ) {
-        return FloatingActionButton(
-          onPressed: () => runMutation({
-            'email': 'multi@multisolution.art.br',
-            'password': 'cPoeiRdmFS',
-          }),
-          tooltip: 'Star',
-          child: Text(result.data.toString()),
-        );
-      },
-      onCompleted: (dynamic result) {
-        print('onComplpeted');
-        print(result.data);
-      },
-    );
+  Future<String> loadAsset() async {
+    return await rootBundle.loadString('assets/mlogin.graphql');
   }
 }
