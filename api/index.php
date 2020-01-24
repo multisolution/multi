6ddda723a42e37aa16ce6ee8c75f18578b21534d
leocavalcante <lc@leocavalcante.com>
2020-01-24 17:12:48 -0300

    Respond only on POST /graphql endpoints

--- api/index.php
+++ api/index.php
@@ -19,6 +19,7 @@ use Siler\Route;
 use Swoole\Runtime;
 use Throwable;
 use function Siler\Encoder\Json\decode;
+use function Siler\Functional\lazy;
 use function Siler\Functional\Monad\maybe;
 use function Siler\GraphQL\{debug, execute, schema, subscriptions_at, subscriptions_manager};
 use function Siler\Swoole\{bearer, cors, http_server_port, json, raw};
@@ -54,24 +55,25 @@ debug($context->debug ? Debug::INCLUDE_DEBUG_MESSAGE | Debug::INCLUDE_TRACE : 0)
 subscriptions_at('ws://localhost:8001');
 
 $handler = function () use ($schema, $root_value, $context, $icsHandler) {
-    if (Route\get('/meetings/{meeting_id}.ics', $icsHandler)) {
-        return;
-    }
+    Route\get('/meetings/{meeting_id}.ics', $icsHandler);
 
-    try {
-        $context->user = maybe(bearer())->bind(function (string $token) use ($context): ?User {
-            $token = JWT::decode($token, $context->appKey, ['HS256']);
-            return $context->db->userById($token->userId);
-        })->return();
+    Route\options('/graphql', lazy(cors));
+    Route\post('/graphql', function () use ($schema, $root_value, $context): void {
+        try {
+            $context->user = maybe(bearer())->bind(function (string $token) use ($context): ?User {
+                $token = JWT::decode($token, $context->appKey, ['HS256']);
+                return $context->db->userById($token->userId);
+            })->return();
 
-        $result = raw() ? execute($schema, decode(raw()), $root_value, $context) : null;
-    } catch (Throwable $exception) {
-        Log\error($exception->getMessage(), ['exception' => $exception, 'trace' => $exception->getTrace()]);
-        $result = FormattedError::createFromException($exception);
-    } finally {
-        cors();
-        json($result);
-    }
+            $result = raw() ? execute($schema, decode(raw()), $root_value, $context) : null;
+        } catch (Throwable $exception) {
+            Log\error($exception->getMessage(), ['exception' => $exception, 'trace' => $exception->getTrace()]);
+            $result = FormattedError::createFromException($exception);
+        } finally {
+            cors();
+            json($result);
+        }
+    });
 };
 
 $manager = subscriptions_manager($schema, [], $root_value, $context);
--- web/lib/apollo.tsx
+++ web/lib/apollo.tsx
@@ -101,7 +101,7 @@ function createApolloClient(initialState: any, getToken: GetToken): ApolloClient
   const fetchOptions = {};
 
   const http = new HttpLink({
-    uri: process.env.API_URL || 'http://localhost:8000',
+    uri: process.env.API_URL || 'http://localhost:8000/graphql',
     fetch,
     fetchOptions
   });
